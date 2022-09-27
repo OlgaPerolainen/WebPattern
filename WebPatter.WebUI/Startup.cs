109961d13380a804c433b78f7440eed04753502d
OlgaPerolainen <95132545+OlgaPerolainen@users.noreply.github.com>
2022-09-27 13:24:26 +0300

    Add files via upload

---
+++ WebPatter.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebPatter.WebUI.Startup))]
+namespace WebPatter.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
